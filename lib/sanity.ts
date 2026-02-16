b9b49addca099cc568560e1f0abebac1fbac83fe
serhattamsen <serhattamsen.26@gmail.com>
2026-02-16 14:36:52 +0300

    fix sanity config

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@sanity/client";
 
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
+  throw new Error("SANITY PROJECT ID missing");
+}
+
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
   useCdn: true,
